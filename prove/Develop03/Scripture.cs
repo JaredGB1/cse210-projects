c6347320ab5c5489791d44a06f59807eed0f4295
JaredGB1 <mjaredgb@gmail.com>
2023-05-19 19:47:18 -0600

    Changes to Scripture.cs

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -21,23 +21,69 @@ public class Scripture
 
     public List<Word> HideWords()
     {
-
         int numberOfWords = _wordsList.Count();
         Random random = new Random();
-        
-        int numberOfRandomWords = random.Next(1,5);
-        for (int i = 0; i < numberOfRandomWords; i++)
+        int visibleWords = VisibleWords();
+        if (visibleWords > 5)
         {
-            int randomIndex = random.Next(numberOfWords);
-            string status2=_wordsList[randomIndex].isHidden();
-            while(status2 =="yes")
+            int numberOfRandomWords = random.Next(1, 5);
+            for (int i = 0; i < numberOfRandomWords; i++)
             {
-                randomIndex = random.Next(numberOfWords);
-                status2=_wordsList[randomIndex].isHidden();          
+                int index = 0;
+                int randomIndex = random.Next(numberOfWords);
+                string status2 = _wordsList[randomIndex].isHidden();
+                if (status2 == "yes")
+                {
+                    while (status2 == "yes")
+                    {
+                        if (index < 27)
+                        {
+                            status2 = _wordsList[index].isHidden();
+                            _wordsList[index].Hide(_wordsList[index]);
+                            index += 1;
+                        }
+                        else
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    _wordsList[randomIndex].Hide(_wordsList[randomIndex]);
+                }
             }
-            _wordsList[randomIndex].Hide(_wordsList[randomIndex]);
-            
         }
+        else
+        {
+            for (int i = 0; i < visibleWords; i++)
+            {
+                int index = 0;
+                int randomIndex = random.Next(numberOfWords);
+                string status2 = _wordsList[randomIndex].isHidden();
+                if (status2 == "yes")
+                {
+                    while (status2 == "yes")
+                    {
+                        if (index < 27)
+                        {
+                            status2 = _wordsList[index].isHidden();
+                            _wordsList[index].Hide(_wordsList[index]);
+                            index += 1;
+                        }
+                        else
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    _wordsList[randomIndex].Hide(_wordsList[randomIndex]);
+                }
+            }
+        }
+
         return _wordsList;
     }
     public void getRenderedText()
@@ -51,23 +97,36 @@ public class Scripture
         Console.WriteLine($"{_reference} {scriptureWords}");
     }
 
-    public string IsCompletelyHidden()
+    public void IsCompletelyHidden()
     {
-        int numberOfVisibleWords=0;
-        string status="";
-        foreach(Word word4 in _wordsList)
+        int numberOfVisibleWords = 0;
+        foreach (Word word4 in _wordsList)
         {
-            string visibility=word4.getVisibility();
-            if (visibility=="Show")
+            string visibility = word4.getVisibility();
+            if (visibility == "Show")
             {
-                numberOfVisibleWords=+1;
+                numberOfVisibleWords = +1;
             }
         }
-        if (numberOfVisibleWords==0)
+        if (numberOfVisibleWords == 0)
         {
             getRenderedText();
-            status="quit";
+            Environment.Exit(0);
         }
-        return status;
+
+    }
+
+    private int VisibleWords()
+    {
+        int numberOfVisibleWords = 0;
+        foreach (Word word4 in _wordsList)
+        {
+            string visibility = word4.getVisibility();
+            if (visibility == "Show")
+            {
+                numberOfVisibleWords= numberOfVisibleWords + 1;
+            }
+        }
+        return numberOfVisibleWords;
     }
 }
