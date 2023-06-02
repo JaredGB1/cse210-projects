ee1d5680357e51468c52a5d9becb768e0f39ea30
JaredGB1 <mjaredgb@gmail.com>
2023-06-02 13:45:42 -0600

    Learning activity

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,14 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title):base(studentName,topic)
+    {
+        _title=title;
+    }
+    public string GetWritingInformation()
+    {
+        string name=GetStudentName();
+        string writingInformation=$"{_title} by {name}";
+        return writingInformation;
+    }
+}
