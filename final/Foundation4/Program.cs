35ced7a4ae54374036ac99776aba7401f1e7f3ca
JaredGB1 <mjaredgb@gmail.com>
2023-07-15 19:27:09 -0600

    Final Changes

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,7 +7,7 @@ class Program
         List<Activity> activityList=new List<Activity>();
         Running running=new Running("14 July 2023",30,4.8);
         activityList.Add(running);
-        Cycling cycling= new Cycling("14 July 2023",60,1);
+        Cycling cycling= new Cycling("14 July 2023",45,2);
         activityList.Add(cycling);
         Swimming swimming= new Swimming("14 July 2023", 30, 10);
         activityList.Add(swimming);
